6437cae904c3555c637731a6fa03ce5e4a94c1e7
Tobias Hasslebrant <tobias.hasslebrant@gmail.com>
2018-02-02 15:17:15 +0100

    removed and renamed files

---
+++ Deploy/publish.ps1
@@ -0,0 +1,20 @@
+Param(
+[string]$version,
+[string]$apikey
+)
+
+$directory = resolve-path .
+if(-not ($directory -match "CtorMock\\Deploy$"))
+{
+    Write-Host "Working directory must be \CtorMock\Deploy\"
+    break
+}
+
+$packages = "CtorMock", "CtorMock.Moq", "\CtorMock.FakeItEasy"
+
+$packages | % {dotnet pack $directory\..\$_\$_.csproj -o $directory\Builds /p:PackageVersion=$version}
+
+nuget setapikey $apikey -source https://api.nuget.org/v3/index.json
+
+$packages | % {dotnet nuget push $directory\Builds\$_.$version.nupkg -s https://api.nuget.org/v3/index.json}
+
--- Deploy/release 1.0.0.0.ps1
+++
@@ -1,9 +0,0 @@
-$version = "1.0.0"
-$directory = resolve-path .
-if(-not ($directory -match "CtorMock\\Deploy$"))
-{
-    Write-Host "Must stand in directory \CtorMock\Deploy\"
-    break
-}
-
-dotnet pack $directory\..\CtorMock\CtorMock.csproj -o $directory\Builds /p:PackageVersion=$version
