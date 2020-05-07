140bd99abd3113ea9ee9e515359c752a33dcc6a9
Tobias Hasslebrant <tobias.hasslebrant@valtech.com>
2020-05-07 11:22:13 +0200

    changed dotnet version, added deploy.sh

---
+++ Deploy/publish.sh
@@ -0,0 +1,22 @@
+
+if [ "$1" == "" ]; then
+    echo 'missing packagename, example CtorMock' 
+    exit 1
+fi
+
+if [ "$2" == "" ]; then
+    echo 'missing version, example 1.0.8'
+     exit 2
+fi
+
+if [ "$3" == "" ]; then
+    echo 'missing apikey'
+    exit 3
+fi
+
+package="$1"
+version="$2"
+apikey="$3"
+
+dotnet pack ../$package/$package.csproj -c Release -o ../Builds /p:PackageVersion=$version
+dotnet nuget push ../Builds/$package.$version.nupkg -s https://api.nuget.org/v3/index.json -k $apikey
