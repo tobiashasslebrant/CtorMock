fcb52b3fdfd845cf8796500a94feeab87abb95f7
Dennis Dyallo <dennis.dyall_wallin@sr.se>
2023-11-07 18:56:31 +0100

    Bump to .NET7

--- CtorMock/ParamReplacing/IParamReplace.cs
+++ CtorMock/ParamReplacing/IParamReplace.cs
@@ -7,8 +7,5 @@ namespace CtorMock.ParamReplacing
     {
         bool CanReplace(ParameterInfo parameterInfo, Type parent);
         object GetReplacement(ParameterInfo parameterInfo, Type parent);
-        //(bool canBeReplaced, Func<object> getReplacement)  Replace(ParameterInfo parameterInfo, Type parent);
-        //(object replaceWith, bool isReplaced) Replace(ParameterInfo parameterInfo, Type parent);
-        
     }
 }
