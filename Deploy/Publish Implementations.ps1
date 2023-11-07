079174ae37b5eb30d5fd1aaa72f08f82560660ef
Dennis Dyallo <dennis.dyall_wallin@sr.se>
2023-11-07 20:07:23 +0100

    update publish implementations

--- Deploy/Publish Implementations.ps1
+++ Deploy/Publish Implementations.ps1
@@ -5,7 +5,7 @@ Param(
 
 #nuget list "CtorMock" -Source https://api.nuget.org/v3/index.json | ? {$_ -match $version}
 
-$packages = "CtorMock.Moq", "CtorMock.FakeItEasy"
+$packages = "CtorMock.Moq", "CtorMock.FakeItEasy", "CtorMock.NSubstitute"
 
 $packages | % { 
     & "$PSScriptRoot\Publish.ps1" -version $version -apikey $apikey -package $_ 
