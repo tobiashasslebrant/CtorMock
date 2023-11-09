19efa78aef3875a6c9f7ccd05a65f816efda10b6
Dennis Dyallo <dennisdyallo@gmail.com>
2023-11-09 09:44:34 +0100

    Added NSubstitute, .NET7 Bump and GitHub Actions (#3)
    * Add NSubstitute
    * Bump to .NET7
    * github actions
    * updated libs

---
+++ CtorMock.NSubstitute/CtorMocker.cs
@@ -0,0 +1,20 @@
+﻿using NSubstitute;
+
+namespace CtorMock.NSubstitute
+{
+    public class CtorMocker : CtorMockerBase
+    {
+        readonly Dictionary<Type, object?> _mocks = new();
+
+        public T? MockOf<T>() where T : class
+            => (T?)_mocks[typeof(T)];
+
+        public override object? CreateMock(Type type)
+        {
+            if (!_mocks.ContainsKey(type))
+                _mocks.Add(type, Substitute.For(new[] {type}, null));
+
+            return _mocks[type];
+        }
+    }
+}
